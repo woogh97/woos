113c1d2c92edfcc4365e183ad606a336792c1acd
woogh97 <sju04015@gmail.com>
2025-02-07 18:01:21 +0900

    BE 테스트트

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,15 @@
 
 <template>
 엔탑회's 놀이터
+
+<button @click="BE연동테스트">BE연동테스트</button>>
 </template>
 
 <script setup>
-
+function BE연동테스트() {
+  console.log('BE연동테스트');
+  fetch('https://a0kvtebzrb.execute-api.ap-southeast-2.amazonaws.com/test')
+    .then((response) => response.json())
+    .then((data) => alert(data));
+}
 </script>
