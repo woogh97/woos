c93273b69a100b3fb8b1b5086204492e104540db
woogh97 <sju04015@gmail.com>
2025-02-10 08:43:57 +0900

    라이엇 연동 테스트트

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,38 @@
 <template>
 엔탑회's 놀이터
 
-<button @click="BE연동테스트">BE연동테스트</button>
+<input v-model="gameName" placeholder="닉네임"/>
+<input v-model="tagLine" placeholder="태그라인"/>
+<button @click="getWorstDamageDealer">마지막 게임 딜량 꼴등 찾기</button>
+
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const gameName = ref();
+const tagLine = ref();
+
+async function getWorstDamageDealer() {
+  try {
+    const res = await fetch(`https://a0kvtebzrb.execute-api.ap-southeast-2.amazonaws.com`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        gameName: gameName.value,
+        tagLine: tagLine.value
+      })
+    });
+
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 function BE연동테스트() {
   console.log('BE연동테스트');
   fetch('https://a0kvtebzrb.execute-api.ap-southeast-2.amazonaws.com/test')
