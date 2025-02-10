ca3728bb0f4076a4bec5a9b8683f13fe39d66230
woogh97 <sju04015@gmail.com>
2025-02-11 00:47:18 +0900

    회원가입 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,10 @@
 <input v-model="tagLine" placeholder="태그라인"/>
 <button @click="getWorstDamageDealer">마지막 게임 딜량 꼴등 찾기</button>
 
+<input type="text" v-model="userId">
+<input type="password" v-model="password">
+<button @click="signup">회원가입</button>
+
 </template>
 
 <script setup>
@@ -34,6 +38,36 @@ async function getWorstDamageDealer() {
     console.log(data);
   } catch (error) {
     console.error(error);
+  }
+}
+
+const userId = ref();
+const password = ref();
+async function signup() {
+  if (!userId || !password) {
+    alert('아이디와 비밀번호는 필수입니다.');
+    return;
+  }
+
+  try {
+    
+    const res = await fetch(`https://a0kvtebzrb.execute-api.ap-southeast-2.amazonaws.com/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userId: userId.value,
+        password: password.value,
+      })
+    });
+
+    const data = await res.json();
+    console.log(data.message);
+    alert(data.message);
+  } catch (error) {
+    console.error(error);
+    alert(error);
   }
 }
 
