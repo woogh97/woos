5aab3b23d9fc73df01784a41f97a6f05d3d13b3e
woogh97 <sju04015@gmail.com>
2025-02-12 09:31:11 +0900

    config.js 수정정

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     VitePWA({
       registerType: "autoUpdate",
+      manifestFilename: "manifest.json",
       manifest: {
         name: "Son of NTOP",
         short_name: "SONT",
