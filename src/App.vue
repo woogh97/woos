3fc793db704cb9cb700f28f055578c73c90fb02f
woogh97 <sju04015@gmail.com>
2025-03-05 08:42:53 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import { useSocketStore } from './stores/socketStore';
+import { ref } from 'vue';
 
 const socketStore = useSocketStore();
 socketStore.connectSocket();
+
+const deferredPrompt = ref(null);
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('beforeinstallprompt Event fired');
+  deferredPrompt.value = e;
+});
 </script>
 
 <template>
+  {{ deferredPrompt ?? '널' }}
   <RouterView />
 </template>
 
