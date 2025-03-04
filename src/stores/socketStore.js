31b90232381061fdfdd2d867e47d7f04b06add07
woogh97 <sju04015@gmail.com>
2025-03-04 19:06:41 +0900

    소켓 재연결결

--- src/stores/socketStore.js
+++ src/stores/socketStore.js
@@ -4,17 +4,30 @@ import { useLoginStore } from './loginStore';
 export const useSocketStore = defineStore('socket', () => {
   const loginStore = useLoginStore();
   let socket = null;
+  let reconnectAttempts = 0;
+  const maxReconnectAttempts = 5;
 
   const connectSocket = () => {
     socket = new WebSocket(`wss://2ker7kkj7f.execute-api.ap-northeast-2.amazonaws.com/production`);
     socket.onopen = () => {
+      reconnectAttempts = 0;
       const userInfo = loginStore.getUserInfo();
       console.log('WebSocket connected');
       socket.send(JSON.stringify({ action: 'registerUser', userId: userInfo.userId }));
     }
     socket.onmessage = (event) => {
+      console.log('WebSocket message start');
       const data = JSON.parse(event.data);
       console.log('WebSocket message received:', data);
+    }
+    socket.onclose = () => {
+      console.log('WebSocket closed');
+      if (reconnectAttempts < maxReconnectAttempts) {
+        setTimeout(() => {
+          reconnectAttempts++;
+          connectSocket();
+        }, 1000);
+      }
     }
   }
 
