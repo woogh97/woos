b65fb6bd5876be099b7d9b8914ae35d99e5248d5
woogh97 <sju04015@gmail.com>
2025-03-05 09:01:58 +0900

    test3

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,27 @@
 import { RouterView } from 'vue-router'
 import { useSocketStore } from './stores/socketStore';
 import { ref } from 'vue';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 const socketStore = useSocketStore();
 socketStore.connectSocket();
 
+const beforeInstallPrompt = ref(null);
 window.addEventListener('beforeinstallprompt', (e) => {
-  debugger;
-  e.prompt();
+  e.preventDefault();
+  beforeInstallPrompt.value = e;
+
+  new Swal({
+    title: '앱 설치',
+    text: '이 앱을 설치하시겠습니까?',
+    showCancelButton: true,
+    confirmButtonText: '네',
+    cancelButtonText: '아니요',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      beforeInstallPrompt.value.prompt();
+    }
+  })
 });
 </script>
 
